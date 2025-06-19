6f130b440416f6c05ff444982ed2d52c4f5bc22f
Scott Tyson <scott.tyson01@gmail.com>
2025-06-19 15:30:47 +1200

    Fix import and function call

--- features/steps/metadata_steps.py
+++ features/steps/metadata_steps.py
@@ -1,11 +1,11 @@
 from behave import when, then
-from databricks_utils import for_each_table, get_table_extended_properties_json
+from databricks_utils import for_each_table, get_table_extended_properties
 
 # NOTE this test is a placeholder for testing useful properties from desc table extended
 @when('I check all tables in "{catalog_schema}" have a managed location')
 def step_check_all_tables_managed_or_comment(context, catalog_schema):
     def check(_, catalog, schema, table):
-        props = get_table_extended_properties_json(context.dbx, catalog, schema, table)
+        props = get_table_extended_properties(context.dbx, catalog, schema, table)
         is_managed = props.get("is_managed_location")
         return is_managed
     for_each_table(context, catalog_schema, check, 'failed_tables')
